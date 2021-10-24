4fc6c02b131d44f5f95ce2f7073ccc186a461718
kvidakov <vidakovic.karlo@gmail.com>
2021-10-24 18:59:46 +0200

    feat(about): add container, row and cols

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -6,14 +6,20 @@ import './about.scss';
 export const AboutComponent = () => {
   return (
     <section className="about">
-      <div className="about__title-img-wrapper">
-        <img className="about__img" src={`${BASE_URL}/assets/images/about-personality-img.png`} alt="iwi woman thinking about personality" />
-        <h2 className="about__title">It’s all about <span className="about__title--highlighted">personality</span></h2>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="about__title-img-wrapper">
+              <img className="about__img" src={`${BASE_URL}/assets/images/about-personality-img.png`} alt="iwi woman thinking about personality" />
+              <h2 className="about__title">It’s all about <span className="about__title--highlighted">personality</span></h2>
+            </div>
+            <p className="about__text"><span className="about__text--highlighted">iwi® matches people</span> based on their personality types.
+              Users are profiled by filling in a fun <span className="about__text--highlighted">Big Five</span> psychological questionnaire — the only test unanimously accepted by the scientific community.
+            </p>
+            <img className="about__big-five-logo" src={`${BASE_URL}/assets/images/big-five-logo-white.svg`} alt="iwi big five logo" />
+          </div>
+        </div>
       </div>
-      <p className="about__text"><span className="about__text--highlighted">iwi® matches people</span> based on their personality types.<br />
-        Users are profiled by filling in a fun <span className="about__text--highlighted">Big Five</span> psychological<br /> questionnaire — the only test unanimously<br />accepted by the scientific community.
-      </p>
-      <img className="about__big-five-logo" src={`${BASE_URL}/assets/images/big-five-logo-white.svg`} alt="iwi big five logo" />
     </section>
   );
 }
