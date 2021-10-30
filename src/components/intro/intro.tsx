fb5477f2e8272fd2ea10f0d0936e4862e3372736
kvidakov <vidakovic.karlo@gmail.com>
2021-10-30 13:50:19 +0200

    style(app): add responsivnes

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -17,8 +17,9 @@ export const IntroComponent = () => {
           <div className="col">
             <img className="intro__logo" src={`${BASE_URL}/assets/images/iwi-logo-white.svg`} alt="iwi white logo" />
             <h1 className="intro__title">Join iwi<span className="intro__registered-trademark">&reg;</span></h1>
-            <h2 className="intro__subtitle">The app that utilizes modern psychology<br/><span className="intro__subtitle--highlighted">to help you build relationships with people<br/>you will easily connect to.</span></h2>
+            <h2 className="intro__subtitle">App that uses <span className="intro__subtitle--highlighted">modern psychology</span> to help you build relationships with people you will easily click with.</h2>
             {/* <Button variant="contained" className="intro__button">Join</Button> */}
+            <p className="intro__available-soon">Available soon on Google Play and App Store</p>
           </div>
         </div>
       </div>
